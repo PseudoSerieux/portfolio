4cca209698782879cea49688856c3cbbf1a524b2
Margaux <margauxretro@gmail.com>
2026-05-10 18:12:26 +0200

    Add experiences + todo for each sections

--- portfolio/src/app/home/home.ts
+++ portfolio/src/app/home/home.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { Apropos } from '../apropos/apropos';
 import { Skills } from '../skills/skills';
+import { Experiences } from '../experiences/experiences';
 import { Projects } from '../projects/projects';
 import { Contact } from '../contact/contact';
 
 @Component({
   standalone: true,
   selector: 'app-home',
-  imports: [Apropos, Skills, Contact, Projects],
+  imports: [Apropos, Experiences, Skills, Contact, Projects],
   templateUrl: './home.html',
   styleUrls: ['./home.scss']
 })
